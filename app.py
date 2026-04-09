25e719efa6ae39476053374d3d58aea238045e56
Hinoki-Sugita <0601yoichiro0601@gmail.com>
2026-04-09 09:00:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ st.write("こんにちは！")
 name = st.text_input("名前を入力してください")
 if name:
     st.write(f"こんにちは、{name}さん！")
+
+present_students = []
+students = ["佐藤", "鈴木", "高橋", "田中", "伊藤"]
+
+for student in students:
+    if st.checkbox(student):
+        present_students.append(student)
+
+st.write("出席者:", present_students)
